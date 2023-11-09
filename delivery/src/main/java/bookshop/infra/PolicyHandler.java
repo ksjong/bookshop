d008b6a0dacf4936a4e4775ffe7ed21103e43ebc
ksjong <142961606+ksjong@users.noreply.github.com>
2023-11-09 23:36:36 +0900

    commit

---
+++ delivery/src/main/java/bookshop/infra/PolicyHandler.java
@@ -0,0 +1,40 @@
+package bookshop.infra;
+
+import bookshop.config.kafka.KafkaProcessor;
+import bookshop.domain.*;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.naming.NameParser;
+import javax.naming.NameParser;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+//<<< Clean Arch / Inbound Adaptor
+@Service
+@Transactional
+public class PolicyHandler {
+
+    @Autowired
+    Repository Repository;
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whatever(@Payload String eventString) {}
+
+    @StreamListener(
+        value = KafkaProcessor.INPUT,
+        condition = "headers['type']=='Ordered'"
+    )
+    public void wheneverOrdered_StartDelivery(@Payload Ordered ordered) {
+        Ordered event = ordered;
+        System.out.println(
+            "\n\n##### listener StartDelivery : " + ordered + "\n\n"
+        );
+
+        // Sample Logic //
+        DeliveryManagement.startDelivery(event);
+    }
+}
+//>>> Clean Arch / Inbound Adaptor
